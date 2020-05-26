26984182c24d34ae0199114794205b29415105e2
stanislav-halyn <stasgalin@gmail.com>
2020-05-26 12:00:04 +0300

    feat: basic socket server setup

---
+++ src/server/constants/index.ts
@@ -0,0 +1,2 @@
+
+export * from './socket-actions.constants';
---
+++ src/server/constants/socket-actions.constants.ts
@@ -0,0 +1,9 @@
+
+export const enum GameActions {
+  SHOW_ALL_BUSINESSES = 'show-all-businesses',
+  SHOW_MY_BUSINESSES = 'show-my-businesses',
+  BUY_BUSINESS = 'buy-business',
+  UPGRADE_BUSINESS = 'upgrade-business',
+  GAIN_CAPITAL = 'gain-capital',
+  GAIN_CAPITAL_COMPLETE = 'gain-capital-complete'
+}
